0da8a87e82d1849223f632b703e89e476057782d
Kerstin <khei.email@gmail.com>
2021-06-08 15:58:39 +0200

    merge with sim

---
+++ sim/src/compile/pySimPulseqSBB.i
@@ -0,0 +1,45 @@
+// pySimPulseqSBB.i
+
+%module pySimPulseqSBB
+
+%{
+#define SWIG_FILE_WITH_INIT
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <map>
+#include <functional>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "ExternalSequence.h"
+#include "SimulationParameters.h"
+#include "SimPulseqSBB.h"
+#include "SimPulseqSBBTemplate.h"
+#include "BlochMcConnellSolver.h"
+%}
+
+%include <typemaps.i>
+%include <std_vector.i>
+%include <std_string.i>
+
+%include <eigen.i>
+
+%template(vectorMatrixXd) std::vector<Eigen::MatrixXd>;
+%template(vectorVectorXd) std::vector<Eigen::VectorXd>;
+
+// Since Eigen uses templates, we have to declare exactly which types we'd
+// like to generate mappings for.
+%eigen_typemaps(Eigen::VectorXd)
+%eigen_typemaps(Eigen::MatrixXd)
+//%eigen_typemaps(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>)
+
+%rename(NoLineshape) None;
+%include "SimulationParameters.h"
+//%include "SimPulseqSBBTemplate.h"
+%include "SimPulseqSBB.h"
+//%template(SimPulseqSBB) SimPulseqSBBTemplate<int>;
+
+
---
+++ sim/src/compile/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+setup.py
+    Setup definitions for distutils package creation
+TODO write setup_ux, setup_os files for pure installation of precompiled shared libraries
+"""
+
+from setuptools import setup, Extension
+import numpy
+from sys import platform
+from pathlib import Path
+
+try:
+    numpy_include = numpy.get_include()
+except AttributeError:
+    numpy_include = numpy.get_numpy_include()
+
+# defining paths to the desired files
+eigen_path = Path('Eigen')
+np_path = Path(numpy_include)
+np_path_np = np_path / 'numpy'
+
+
+SimPulseqSBB_module = Extension(name='_pySimPulseqSBB',
+                                sources=['pySimPulseqSBB.i', 'SimPulseqSBB.cpp', 'SimulationParameters.cpp',
+                                         'ExternalSequence.cpp'],
+                                include_dirs=[eigen_path, np_path, np_path_np],
+                                extra_compile_args=['-Xpreprocessor', '-fopenmp'],
+                                swig_opts=['-c++'],
+                                language='c++'
+                                )
+
+setup(name='pySimPulseqSBB',
+      version='0.1',
+      author="K. Heinecke",
+      author_email='k.heinecke@campus.tu-berlin.de',
+      description="Python package to use the C++ code SimPulseqSBB for pulseq-CEST simulations.",
+      ext_modules=[SimPulseqSBB_module],
+      py_modules=["pySimPulseqSBB"],
+      )
+
+
+
