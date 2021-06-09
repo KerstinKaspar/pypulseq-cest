e0af23af58790e2b35647dfd57ddec4c192cfe57
Kerstin <k.heinecke@campus.tu-berlin.de>
2021-06-09 10:31:14 +0200

    typos

--- simulate.py
+++ simulate.py
@@ -8,14 +8,14 @@ from pypulseq_cest.simulate import simulate
 sim_config = 'pypulseq_cest/example_library/config_example.yaml'
 seq_file = 'pypulseq_cest/example_library/seq_example.seq'
 
-# Run the simulateions with a
+# Run the simulateions with:
 sim = simulate(config_file=sim_config, seq_file=seq_file, show_plot=False)
 
-# If you need additional processing, you can access the sunctions directly from the simulation object:
-# For example, you can retrieve the magnetization vector with the following function:
+# If you need additional processing, you can access the functions directly from the simulation object.
+# For example, you can retrieve the magnetization vector with:
 m_out = sim.GetFinalMagnetizationVectors()
 
-# To get the correct magnetization in z-direction, you can use the following function.
+# To get the correct magnetization in z-direction, you can use:
 offsets, mz = get_zspec(m_out=m_out, sp=sim, seq_file=seq_file, normalize_if_m0=True)
 
 # You can plot the magnetization with the following function from the bmctool:
