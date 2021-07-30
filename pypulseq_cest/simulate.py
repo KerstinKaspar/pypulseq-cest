1205b983e89479830e6d56f758b8e03bb7797362
Patrick Schuenke <patrick.schuenke@gmail.com>
2021-07-30 11:21:13 +0200

    update simulate function

--- pypulseq_cest/simulate.py
+++ pypulseq_cest/simulate.py
@@ -3,7 +3,7 @@ simulate.py
 """
 from typing import Union
 from pathlib import Path
-from pySimPulseqSBB import SimPulseqSBB, SimulationParameters
+from pySimPulseqSBB import BMCSim, SimulationParameters
 from pypulseq_cest.parser import parse_params, get_zspec
 from bmctool.set_params import load_params
 from bmctool.utils.eval import plot_z
@@ -24,14 +24,17 @@ def simulate(config_file: Union[str, Path],
     # load the parameters
     sp = load_params(config_file)
 
-    # parse for C++ handling
+    # create SWIG object of type SimulationParameters (C++ class)
     sim_params = parse_params(sp=sp, seq_file=seq_file)
 
-    # run the simulation
-    SimPulseqSBB(sim_params, str(seq_file))
+    # create SWIG object of type BMCSim (C++ class)
+    sim = BMCSim(sim_params, str(seq_file))
+
+    # run simulation
+    sim.RunSimulation()
 
     # retrieve the calculated magnetization
-    m_out = sim_params.GetFinalMagnetizationVectors()
+    m_out = sim.GetFinalMagnetizationVectors()
 
     if show_plot:
         if 'offsets' in kwargs:
@@ -42,6 +45,7 @@ def simulate(config_file: Union[str, Path],
 
         plot_z(mz=mz,
                offsets=offsets,
+               plot_mtr_asym=False,
                **kwargs)
 
     return sim_params
@@ -63,4 +67,3 @@ def sim_example():
 
 if __name__ == '__main__':
     sim_example()
-
